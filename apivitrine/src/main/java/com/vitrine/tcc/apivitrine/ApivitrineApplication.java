abca165804c572a114ea9f10717c8bae32dec2d5
deathSWAG #swag #hashtag <rm99566@estudante.fieb.edu.br>
2026-06-18 11:35:38 +0000

    Criação do projeto react vite

---
+++ apivitrine/src/main/java/com/vitrine/tcc/apivitrine/ApivitrineApplication.java
@@ -0,0 +1,13 @@
+package com.vitrine.tcc.apivitrine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApivitrineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApivitrineApplication.class, args);
+	}
+
+}
---
+++ apivitrine/src/test/java/com/vitrine/tcc/apivitrine/ApivitrineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitrine.tcc.apivitrine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApivitrineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
